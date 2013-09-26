42de33048d27f0d016bafd0fcc587de7e582e99c
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-09-25 21:38:03 -0400

    Removed deleted files

--- db/migrate/20130919191806_create_product_wishlists.rb
+++
@@ -1,8 +0,0 @@
-class CreateProductWishlists < ActiveRecord::Migration
-  def change
-    create_table :product_wishlists do |t|
-      t.integer :wishlist_id
-      t.integer :product_id
-    end
-  end
-end
