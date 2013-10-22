afbc1f3b52f4e6254789febb43f51c631518bea7
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-10-22 11:59:32 -0400

    changed some navbar text, linked products to user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
     @products = Product.where(:user_id => params[:id])
     @json = Product.where(:user_id => params[:id]).to_gmaps4rails
 
+    email = @user.email
+    @url_hash = Digest::MD5.hexdigest(email)
   end
 
 end
